374861ee084cce91f261adc86790ca5514b7bd53
nawresncib12 <nawresncib@insat.u-carthage.tn>
2023-06-21 21:02:42 +0100

    added publishing data to new topic

--- src/mqtt.controller.ts
+++ src/mqtt.controller.ts
@@ -1,21 +1,35 @@
 import { Controller } from '@nestjs/common';
 import {
+  ClientProxyFactory,
   Ctx,
   MessagePattern,
   MqttContext,
   Payload,
+  Transport,
 } from '@nestjs/microservices';
 
 @Controller('math')
 export class MqttController {
-  //testing notifications
-  @MessagePattern('temperature_channel')
-  getNotifications(@Payload() data: string, @Ctx() context: MqttContext) {
-    const pack = context.getPacket();
-    console.log(pack);
-    return `Received : ${data} from topic ${context.getTopic()}`;
-  }
+  private mqttClient: any;
 
+  constructor() {
+    this.mqttClient = ClientProxyFactory.create({
+      transport: Transport.MQTT,
+      options: {
+        url: 'mqtt://localhost:1883',
+      },
+    });
+  }
+  @MessagePattern('temperature_channel')
+  async getTemperature(@Payload() data: number, @Ctx() context: MqttContext) {
+    const newTemp = data + 20;
+    const newData = `Received: ${data} from topic ${context.getTopic()} and changed it to ${newTemp}`;
+
+    // Publish the new data to a new topic
+    await this.mqttClient.emit('processed_temperature_channel', newTemp);
+
+    return newData;
+  }
   //using an object
   @MessagePattern('device')
   getDevice(@Payload() data, @Ctx() context: MqttContext): string {
